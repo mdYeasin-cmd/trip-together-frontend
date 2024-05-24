aee62cf119fffe65b5bf2794ede601d725574b32
mdYeasin-cmd <yeasin200011@gmail.com>
2024-05-24 16:51:38 +0600

    Add search section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,6 +1,6 @@
 import Footer from "@/components/Shared/Footer/Footer";
 import HeroSection from "@/components/UI/HomePage/HeroSection/HeroSection";
-import SearchSection from "@/components/UI/SearchSection/SearchSection";
+import SearchSection from "@/components/UI/HomePage/SearchSection/SearchSection";
 
 export default function HomePage() {
   return (
---
+++ src/components/UI/HomePage/SearchSection/SearchSection.tsx
@@ -0,0 +1,38 @@
+import {
+  Button,
+  Container,
+  Grid,
+  MenuItem,
+  Stack,
+  TextField,
+} from "@mui/material";
+import React from "react";
+
+const SearchSection = () => {
+  return (
+    <Container>
+      <Stack sx={{ py: 5 }}>
+        <Grid container gap={2}>
+          <Grid item md={6}>
+            <TextField
+              placeholder="Search you desire travell buddy"
+              sx={{ width: "100%" }}
+            />
+          </Grid>
+          <Grid item md={2}>
+            <TextField select sx={{ width: "100%" }}>
+              <MenuItem value={"Destination"}>Destination</MenuItem>
+              <MenuItem value={"Travel dates"}>Travel dates</MenuItem>
+              <MenuItem value={"Travel type"}>Travel type</MenuItem>
+            </TextField>
+          </Grid>
+          <Grid item md={2}>
+            <Button sx={{ width: "100%", height: "100%" }}>Search</Button>
+          </Grid>
+        </Grid>
+      </Stack>
+    </Container>
+  );
+};
+
+export default SearchSection;
